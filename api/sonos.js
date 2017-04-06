d9e0fc90eedb09026959a5a154298c3ead468f0a
Sam Piggott <sam@raw-pixel.com>
2017-04-06 16:34:28 +0100

    Added additional sonos functionality

--- api/sonos.js
+++ api/sonos.js
@@ -22,7 +22,7 @@ export const sonosCurrentTrack = function () {
 
 	}).then(function (track) {
 
-		return {success: true, track: track};
+		return {success: true, result: track};
 
 	}, function (err) {
 
@@ -37,15 +37,92 @@ export const sonosCurrentTrack = function () {
 
 }
 
+export const sonosTurnItUp = function () {
+
+	return Q().then(function () {
+
+		if (!seconds) { throw new Error("No seconds supplied."); }
+
+		const deferred = Q.defer();
+
+		sonos.setVolume(60, function (err, result) {
+
+			if (err) { deferred.reject(err); return; }
+			deferred.resolve(result);
+
+		});
+
+		return deferred.promise;
+
+	}, function (err) {
+
+		throw err;
+
+	}).then(function (result) {
+
+		return {success: true, result: result};
+
+	}).catch(function (err) {
+
+		return {success: false, error: err.message};
+
+	});
+
+}
+
+export const sonosSeekToSeconds = function (seconds) {
+
+	return Q().then(function () {
+
+		if (!seconds) { throw new Error("No seconds supplied."); }
+
+		const deferred = Q.defer();
+
+		sonos.seek(seconds, function (err, result) {
+
+			if (err) { deferred.reject(err); return; }
+			deferred.resolve(result);
+
+		});
+
+		return deferred.promise;
+
+	}, function (err) {
+
+		throw err;
+
+	}).catch(function (err) {
+
+		return {success: false, error: err.message};
+
+	});
+
+}
+
 export const sonosPlayURI = function (uri) {
 
 	return Q().then(function () {
 
 		if (!uri) { throw new Error("No URI supplied."); }
 
+		// Make sure the queue is currently selected...
+
 		const deferred = Q.defer();
 
+		sonos.selectQueue(function (err, result) {
+
+			if (err) { deferred.reject(err); return; }
+			deferred.resolve(result);
+
+		});
+
+		return deferred.promise;
+
+	}).then(function (success) {
+
 		// Add Spotify track to the queue next...
+
+		const deferred = Q.defer();
 
 		sonos.addSpotifyQueue(uri, function (err, result) {
 
@@ -56,14 +133,37 @@ export const sonosPlayURI = function (uri) {
 
 		return deferred.promise;
 
+	}, function (err) {
+
+		throw err;
+
 	}).then(function (result) {
 
 		const deferred = Q.defer();
-	
+
 		// ...then just skip to that queued track immediately. 
 		// Fuck the haters.
 
 		sonos.next(function (err, success) {
+
+			if (err) { deferred.reject(err); return; }
+			deferred.resolve(result);
+
+		});
+
+		return deferred.promise;
+
+	}, function (err) {
+
+		throw err;
+
+	}).then(function (result) {
+
+		const deferred = Q.defer();
+
+		// ...and start playing. Breathe.
+
+		sonos.play(function (err, success) {
 
 			if (err) { deferred.reject(err); return; }
 			deferred.resolve(result);
